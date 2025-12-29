9614d647a2d6333871e78c48c9830dc45cd826e0
Enes Yilmaz <enesyilmaz5157@gmail.com>
2025-12-28 20:33:16 -0500

    added fix for improper color detection

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ import cv2
 from utils import get_limits
 
 
-yellow = [0, 255, 255]
+targetColor = [0, 0, 0]  # BGR format
 cap = cv2.VideoCapture(0)
 
 while True:
 
     ret, frame = cap.read()
+    if not ret:
+        continue
 
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 
 
-    lower_limit, upper_limit = get_limits(yellow)
+    lower_limit, upper_limit = get_limits(targetColor)
 
     mask = cv2.inRange(hsvImage, lower_limit, upper_limit)
 
-    cv2.imshow('frame', frame)
+    # cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- utils.py
+++ utils.py
@@ -4,11 +4,12 @@ import cv2
 
 def get_limits(color):
 
-
     c = np.uint8([[color]])
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-    
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
-    upperLimit = hsvC[0][0][0] + 10, 255, 255
+    h = int(hsvC[0, 0, 0])
+
+    # Clip hue to [0,179] and build uint8 arrays for cv2.inRange
+    lowerLimit = np.array([max(h - 10, 0), 100, 100], dtype=np.uint8)
+    upperLimit = np.array([min(h + 10, 179), 255, 255], dtype=np.uint8)
 
     return lowerLimit, upperLimit
